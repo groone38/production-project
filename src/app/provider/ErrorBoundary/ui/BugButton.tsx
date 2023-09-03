57029dfbfd4626f04d60174745e354ba6ee125bd
groone38 <groone38@yandex.ru>
2023-09-03 14:05:36 +0300

    add bundle analiz

--- src/app/provider/ErrorBoundary/ui/BugButton.tsx
+++ src/app/provider/ErrorBoundary/ui/BugButton.tsx
@@ -4,11 +4,7 @@ import Button from "shared/ui/Button/Button";
 const BugButton = () => {
   const [error, setError] = useState(false);
   const onTrow = () => setError(true);
-  useEffect(() => {
-    if (error) {
-      throw new Error();
-    }
-  }, [error]);
+
   return <Button onClick={onTrow}>Throw error</Button>;
 };
 
